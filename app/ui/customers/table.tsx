6b2d295ccaa9a94192c4f8a3c5c42905d6a57e02
carlosvfb <158277340+carlosvfb@users.noreply.github.com>
2024-07-16 14:09:28 -0300

    first commit

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -25,7 +25,7 @@ export default async function CustomersTable({
                 {customers?.map((customer) => (
                   <div
                     key={customer.id}
-                    className="mb-2 w-full rounded-md bg-white p-4"
+                    className="mb-2 w-full rounded-md bg-white:status === 'painding' p-4"
                   >
                     <div className="flex items-center justify-between border-b pb-4">
                       <div>
